a8ba94ed29017867d580d0490d0274d19e00c11c
Molpe <fran.mscr@gmail.com>
2022-05-08 19:07:01 +0200

    feat: added movies pattern for associated queries

---
+++ sql-node-slonik/exercises/movies.js
@@ -0,0 +1,65 @@
+const db = require('../configs/db')
+const { sql } = require('slonik')
+
+module.exports = {
+    // Your movies queries
+    async q9() {
+        // README - Movies.9
+    },
+
+    async q10() {
+        // README - Movies.10
+    },
+
+    async q11() {
+        // README - Movies.11
+    },
+
+    async q12() {
+        // README - Movies.12
+    },
+
+    async q13() {
+        // README - Movies.13
+    },
+
+    async q14() {
+        // README - Movies.14
+    },
+
+    async q15() {
+        // README - Movies.15
+    },
+
+    async q16() {
+        // README - Movies.16
+    },
+
+    async q17() {
+        // README - Movies.17
+    },
+
+    async q18() {
+        // README - Movies.18
+    },
+
+    async q19() {
+        // README - Movies.19
+    },
+
+    async q20() {
+        // README - Movies.20
+    },
+
+    async q21() {
+        // README - Movies.21
+    },
+
+    async q22() {
+        // README - Movies.22
+    },
+
+    async q23() {
+        // README - Movies.23
+    },
+}
